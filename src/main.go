573cc48b4e75ee6164d79b8855df0f14c5c261e7
thatpix3l <37268984+thatpix3l@users.noreply.github.com>
2020-12-18 01:19:03 -0500

    Add logic to determine if file exists or not

---
+++ src/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"flag"
+	"path/filepath"
+)
+
+func main() {
+	fileNamePtr := flag.String("file", "", "Path to file")
+	flag.Parse()
+	fileName := *fileNamePtr
+
+	i := 0
+	for fileExists(fileName) {
+		fmt.Println(filepath.Base(fileName))
+		fileName = "literally anything else"
+		i++
+	}
+}
+
+func fileExists(fileInput string) bool {
+	_, errStat := os.Stat(fileInput)
+  doesExist := true
+	if errStat != nil {
+		return false
+	}
+	return doesExist
+}
