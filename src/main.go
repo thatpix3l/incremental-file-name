f74e1e224f79186228d95462407229a27eff0471
thatpix3l <37268984+thatpix3l@users.noreply.github.com>
2020-12-17 23:43:26 -0500

    Add new flag and handle empty values for flags

--- src/main.go
+++ src/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	// Flags
 	textPtr := flag.String("text", "", "Text to parse.")
+	portPtr := flag.String("port", "", "Port number.")
 	flag.Parse()
 
-	fmt.Printf("%s", *textPtr)
+	// Screw around with missing flag values and how to handle them
+	if *textPtr == "" || *portPtr == "" {
+		fmt.Printf("You didn't provide any options.\nNow you will face the consequences...")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Text you typed: %s\nPort you typed: %s", *textPtr, *portPtr)
 }
